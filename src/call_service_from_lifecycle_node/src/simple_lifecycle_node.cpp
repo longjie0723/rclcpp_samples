c649bb50da5e8b7711269d750f34f8564f5c203f
XiandiShan <shan.xiandi@techmagic.co.jp>
2024-11-29 15:49:50 +0900

    updated README

--- src/call_service_from_lifecycle_node/src/simple_lifecycle_node.cpp
+++ src/call_service_from_lifecycle_node/src/simple_lifecycle_node.cpp
@@ -1,4 +1,4 @@
-#include "service/add_two_ints_client.hpp"
+#include "call_service_from_lifecycle_node/add_two_ints_client.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
@@ -9,18 +9,23 @@ public:
     SimpleLifecycleNode(const rclcpp::NodeOptions &options)
     : rclcpp_lifecycle::LifecycleNode("simple_lifecycle_node", options)
     {
-        client_ = std::make_unique<LifecycleClientNode>(shared_from_this());
-
-        auto handle_add_two_ints =
-        [this](const std::shared_ptr<std_srvs::srv::Empty::Request> request,
-            std::shared_ptr<std_srvs::srv::Empty::Response> response) -> void
-        {
-            RCLCPP_INFO(get_logger(), "Execute simple lifecycle callback");
-            client_->execute();
-            RCLCPP_INFO(get_logger(), "Finished simple lifecycle callback.");
-        };
-        srv_ = this->create_service<std_srvs::srv::Empty>("/simple_lifecycle_node/add_two_ints", handle_add_two_ints);
+        srv_ = this->create_service<std_srvs::srv::Empty>(
+            "/simple_lifecycle_node/add_two_ints", 
+            std::bind(&SimpleLifecycleNode::simple_callback, this, std::placeholders::_1, std::placeholders::_2));
     };
+
+    void initialize()
+    {
+        client_ = std::make_unique<LifecycleClientNode>(shared_from_this());
+    }
+
+    void simple_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+                   const std::shared_ptr<std_srvs::srv::Empty::Response> response)
+    {
+        RCLCPP_INFO(this->get_logger(), "Execute simple lifecycle node callback.");
+        client_->execute();
+        RCLCPP_INFO(this->get_logger(), "Finished simple lifecycle node callback.");
+    }
 
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
     
@@ -65,15 +70,17 @@ private:
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_;
 };
 
-// int main(int argc, char * argv[])
-// {
-//     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-//     rclcpp::init(argc, argv);
-//     rclcpp::executors::MultiThreadedExecutor executor;
-//     rclcpp::NodeOptions options;
-//     std::shared_ptr<SimpleLifecycleNode> node = std::make_shared<SimpleLifecycleNode>(options);
-//     executor.add_node(node->get_node_base_interface());
-//     executor.spin();
-// }
+int main(int argc, char * argv[])
+{
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions options;
+    std::shared_ptr<SimpleLifecycleNode> node = std::make_shared<SimpleLifecycleNode>(options);
+    node->initialize();
 
-RCLCPP_COMPONENTS_REGISTER_NODE(SimpleLifecycleNode)
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node->get_node_base_interface());
+    executor.spin();
+}
+
+// RCLCPP_COMPONENTS_REGISTER_NODE(SimpleLifecycleNode)
---
+++ src/call_service_from_node/include/call_service_from_node/add_two_ints_client.hpp
@@ -0,0 +1,60 @@
+#include <chrono>
+#include <cinttypes>
+#include <memory>
+#include <thread>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+
+using namespace std::chrono_literals;
+
+class ClientNode
+{
+public:
+    ClientNode(const std::shared_ptr<rclcpp::Node>& node)
+        : node_(node)
+    {}
+
+    void execute()
+    {
+        auto client_cb_group_ = node_->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        client_ = node_->create_client<example_interfaces::srv::AddTwoInts>("/add_two_ints", rmw_qos_profile_services_default, client_cb_group_);
+        RCLCPP_INFO(node_->get_logger(), "Execute /add_two_ints");
+
+        while(!client_->wait_for_service(std::chrono::seconds(5))) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(node_->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                return;
+            }
+        }
+        
+        auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+        request->a = 1;
+        request->b = 2;
+
+        // [test 1]
+        using ServiceResponseFuture = rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedFuture;
+        auto response_received_callback = [this](ServiceResponseFuture future) {
+            RCLCPP_INFO(this->node_->get_logger(), "Execute callback.");
+            auto result = future.get();
+            RCLCPP_INFO(this->node_->get_logger(), "Result: %ld", result->sum);
+        };
+        auto future_result = client_->async_send_request(request, response_received_callback);
+        RCLCPP_INFO(node_->get_logger(), "Finished /add_two_ints.");
+
+        // [test 2]
+        // auto future = client_->async_send_request(request);
+        // if (future.wait_for(std::chrono::seconds(3)) != std::future_status::timeout)  {
+        //     const auto &response = future.get();
+        //     RCLCPP_INFO(node_->get_logger(), "Service call succeeded, response: %ld", response->sum);
+        // } else {
+        //     RCLCPP_ERROR(node_->get_logger(), "Failed to call /add_two_ints");
+        // }
+    }
+
+private:
+    std::shared_ptr<rclcpp::Node> node_;
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client_;
+};
