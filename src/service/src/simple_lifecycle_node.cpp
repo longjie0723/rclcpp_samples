d9caaf057f72bfe6da1c686884f4890fc4744cbc
XiandiShan <shan.xiandi@techmagic.co.jp>
2024-11-29 13:42:26 +0900

    added simple node

--- src/service/src/simple_lifecycle_node.cpp
+++ src/service/src/simple_lifecycle_node.cpp
@@ -1,33 +1,32 @@
-#include "call_service_in_lifecycle_node/add_two_ints_client.hpp"
+#include "service/add_two_ints_client.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
 
-class SimpleLifecycle : public rclcpp_lifecycle::LifecycleNode
+class SimpleLifecycleNode : public rclcpp_lifecycle::LifecycleNode
 {
 public:
-    SimpleLifecycle(const rclcpp::NodeOptions &options)
-    : rclcpp_lifecycle::LifecycleNode("simple_lifecycle", options)
+    SimpleLifecycleNode(const rclcpp::NodeOptions &options)
+    : rclcpp_lifecycle::LifecycleNode("simple_lifecycle_node", options)
     {
         auto handle_add_two_ints =
         [this](const std::shared_ptr<std_srvs::srv::Empty::Request> request,
             std::shared_ptr<std_srvs::srv::Empty::Response> response) -> void
         {
-            RCLCPP_INFO(get_logger(), "Execute /simple_lifecycle/add_two_ints.");
-            client = std::make_unique<ClientNode>(shared_from_this());
-            client->execute();
-            RCLCPP_INFO(get_logger(), "Finished /simple_lifecycle/add_two_ints.");
+            client_ = std::make_unique<LifecycleClientNode>(shared_from_this());
+            RCLCPP_INFO(get_logger(), "Execute simple lifecycle callback");
+            client_->execute();
+            RCLCPP_INFO(get_logger(), "Finished simple lifecycle callback.");
         };
-        srv_ = this->create_service<std_srvs::srv::Empty>("/simple_lifecycle/add_two_ints", handle_add_two_ints);
+        srv_ = this->create_service<std_srvs::srv::Empty>("/simple_lifecycle_node/add_two_ints", handle_add_two_ints);
     };
 
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
     
     CallbackReturn on_configure(const rclcpp_lifecycle::State &)
     {
-        client = std::make_unique<ClientNode>(shared_from_this());
         RCLCPP_INFO(get_logger(), "Configuring");
-        client->execute();
+        client_->execute();
         return CallbackReturn::SUCCESS;
     }
     
@@ -61,20 +60,19 @@ public:
         return CallbackReturn::SUCCESS;
     };
 private:
-    std::unique_ptr<ClientNode> client;
+    std::unique_ptr<LifecycleClientNode> client_;
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_;
-    
 };
 
-int main(int argc, char * argv[])
-{
-    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-    rclcpp::init(argc, argv);
-    rclcpp::executors::MultiThreadedExecutor executor;
-    rclcpp::NodeOptions options;
-    std::shared_ptr<SimpleLifecycle> node = std::make_shared<SimpleLifecycle>(options);
-    executor.add_node(node->get_node_base_interface());
-    executor.spin();
-}
+// int main(int argc, char * argv[])
+// {
+//     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+//     rclcpp::init(argc, argv);
+//     rclcpp::executors::MultiThreadedExecutor executor;
+//     rclcpp::NodeOptions options;
+//     std::shared_ptr<SimpleLifecycleNode> node = std::make_shared<SimpleLifecycleNode>(options);
+//     executor.add_node(node->get_node_base_interface());
+//     executor.spin();
+// }
 
-// RCLCPP_COMPONENTS_REGISTER_NODE(SimpleLifecycle)
+RCLCPP_COMPONENTS_REGISTER_NODE(SimpleLifecycleNode)
---
+++ src/service/src/simple_node.cpp
@@ -0,0 +1,42 @@
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "service/add_two_ints_client.hpp"
+
+class SimpleNode : public rclcpp::Node
+{
+public:
+    explicit SimpleNode(const rclcpp::NodeOptions & options)
+    : Node("simple_node", options)
+    {
+        auto simple_callback = 
+            [this](const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+                   const std::shared_ptr<std_srvs::srv::Empty::Response> response) -> void
+            {
+                client_ = std::make_unique<ClientNode>(shared_from_this());
+                RCLCPP_INFO(this->get_logger(), "Execute simple callback.");
+                client_->execute();
+                RCLCPP_INFO(this->get_logger(), "Finished simple callback.");
+            };
+
+        srv_ = this->create_service<std_srvs::srv::Empty>("/simple_node/add_two_ints", simple_callback);
+    }
+
+private:
+    std::unique_ptr<ClientNode> client_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_;
+};
+
+// int main(int argc, char * argv[])
+// {
+//     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+//     rclcpp::init(argc, argv);
+//     rclcpp::executors::MultiThreadedExecutor executor;
+//     rclcpp::NodeOptions options;
+//     std::shared_ptr<SimpleNode> node = std::make_shared<SimpleNode>(options);
+//     executor.add_node(node->get_node_base_interface());
+//     executor.spin();
+// }
+
+// Register the node as a component
+RCLCPP_COMPONENTS_REGISTER_NODE(SimpleNode)
